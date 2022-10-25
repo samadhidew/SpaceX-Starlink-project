2d6e49c62a25d1efd3290225e53c5610a886f62d
Samadhi Dewmini <samadhidew@gmail.com>
2022-10-25 14:06:23 +0530

    Add the code

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace WFA_Lab_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbox_sataliteID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tboxSNAME = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tboxLongi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tboxLatitude = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tboxElivation = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txbhealth = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Satelite ID";
+            // 
+            // tbox_sataliteID
+            // 
+            this.tbox_sataliteID.Location = new System.Drawing.Point(167, 26);
+            this.tbox_sataliteID.Name = "tbox_sataliteID";
+            this.tbox_sataliteID.Size = new System.Drawing.Size(327, 20);
+            this.tbox_sataliteID.TabIndex = 1;
+            this.tbox_sataliteID.TextChanged += new System.EventHandler(this.tbox_sataliteID_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Satelite Name";
+            // 
+            // tboxSNAME
+            // 
+            this.tboxSNAME.Location = new System.Drawing.Point(167, 57);
+            this.tboxSNAME.Name = "tboxSNAME";
+            this.tboxSNAME.Size = new System.Drawing.Size(592, 20);
+            this.tboxSNAME.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Longitude";
+            // 
+            // tboxLongi
+            // 
+            this.tboxLongi.Location = new System.Drawing.Point(167, 91);
+            this.tboxLongi.Name = "tboxLongi";
+            this.tboxLongi.Size = new System.Drawing.Size(218, 20);
+            this.tboxLongi.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(409, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Latitude";
+            // 
+            // tboxLatitude
+            // 
+            this.tboxLatitude.Location = new System.Drawing.Point(522, 94);
+            this.tboxLatitude.Name = "tboxLatitude";
+            this.tboxLatitude.Size = new System.Drawing.Size(237, 20);
+            this.tboxLatitude.TabIndex = 8;
+            this.tboxLatitude.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Elevation";
+            // 
+            // tboxElivation
+            // 
+            this.tboxElivation.Location = new System.Drawing.Point(138, 123);
+            this.tboxElivation.Name = "tboxElivation";
+            this.tboxElivation.Size = new System.Drawing.Size(218, 20);
+            this.tboxElivation.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(380, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 21);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Health Status";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(12, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(253, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(497, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(237, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(283, 416);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Clear All";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txbhealth
+            // 
+            this.txbhealth.Location = new System.Drawing.Point(518, 123);
+            this.txbhealth.Name = "txbhealth";
+            this.txbhealth.Size = new System.Drawing.Size(241, 20);
+            this.txbhealth.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Salmon;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 203);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 182);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(522, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txbhealth);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tboxElivation);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tboxLatitude);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tboxLongi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tboxSNAME);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbox_sataliteID);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbox_sataliteID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tboxSNAME;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tboxLongi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tboxLatitude;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tboxElivation;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txbhealth;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WFA_Lab_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int satid = int.Parse(tbox_sataliteID.Text);
+            string satname = tboxSNAME.Text;
+            int longitite = int.Parse(tboxLongi.Text);
+            int latitute = int.Parse(tboxLatitude.Text);
+            float elevation = int.Parse(tboxElivation.Text);
+            string health = txbhealth.Text;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\c#\Database.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            string qry = "UPDATE satalite SET satid='" + satid + "',satname='" + satname + "',longitite='" + longitite + "',latitute='" + latitute + "',elevation='" + elevation + "',health='" + health + "' from satalite WHERE satid = " + satid + " ";
+            SqlCommand cmd = new SqlCommand(qry, con);
+            cmd.ExecuteNonQuery();
+            
+            string Query = "select * from satalite ";
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, con);
+            DataSet set = new DataSet();
+            
+            adapter.Fill(set, "satalite");
+            dataGridView1.DataSource = set.Tables["satalite"];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int satid = int.Parse(tbox_sataliteID.Text);
+            string satname = tboxSNAME.Text;
+            int longitite = int.Parse(tboxLongi.Text);
+            int latitute = int.Parse(tboxLatitude.Text);
+            float elevation = int.Parse(tboxElivation.Text);
+            string health = txbhealth.Text;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\c#\Database.mdf;Integrated Security=True;Connect Timeout=30");
+
+
+
+
+            try
+            {
+                con.Open();
+                string query = "INSERT INTO satalite values (" + satid + ", '" + satname + "'," + longitite + "," + latitute + "," + elevation + ",'" + health + "')";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Values added Successfully");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong" + ex.ToString());
+            }
+            string Query = "select * from satalite ";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, con);
+            DataSet set = new DataSet();
+
+            adapter.Fill(set, "satalite");
+            dataGridView1.DataSource = set.Tables["satalite"];
+        }
+
+        private void Updatetable()
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void tbox_sataliteID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tbox_sataliteID.Text = "";
+            tboxSNAME.Text = "";
+            tboxLongi.Text = "";
+            tboxLatitude.Text = "";
+            tboxElivation.Text = "";
+            txbhealth.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\c#\Database.mdf;Integrated Security=True;Connect Timeout=30");
+            int satid = int.Parse(tbox_sataliteID.Text);
+            con.Open();
+            String del = "DELETE FROM  satalite where satid =" + satid + "";
+            SqlCommand cmd = new SqlCommand(del, con);
+            cmd.ExecuteNonQuery();
+            string Query = "select * from satalite ";
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, con);
+            DataSet set = new DataSet();
+
+            adapter.Fill(set, "satalite");
+            dataGridView1.DataSource = set.Tables["satalite"];
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            if (tbox_sataliteID.Text != "")
+            { 
+
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\c#\Database.mdf;Integrated Security=True;Connect Timeout=30");
+                 SqlCommand cmd = new SqlCommand("SELECT satid,satname,longitite,latitute,elevation,health from satalite where satid = @satid", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@satid", int.Parse(tbox_sataliteID.Text));
+                SqlDataReader da = cmd.ExecuteReader();
+                 while (da.Read())
+            {
+                tboxSNAME.Text = da.GetValue(1).ToString();
+                tboxLongi.Text = da.GetValue(2).ToString();
+                tboxLatitude.Text = da.GetValue(3).ToString();
+                tboxElivation.Text = da.GetValue(4).ToString();
+                txbhealth.Text = da.GetValue(5).ToString();
+            }
+                con.Close();
+            }
+        }
+    }
+}
